a26d3d3b1eed78907e2f343b2c9cc5495e4f41e5
Marvin Alejandro Herrera Vizuett <marvinahv@gmail.com>
2020-09-16 09:19:44 +0000

    Feature: Mentor can see signup form

---
+++ features/mentor_can_see_signup_form.feature
@@ -0,0 +1,7 @@
+@feature-mentor-can-see-signup-form
+Feature: Mentor can see signup form
+  
+  Scenario: Visitor goes to signup page
+    Given Visitor is not a logged-in user
+    When he goes to the signup page
+    Then he should see the signup form
---
+++ features/steps/mentor_can_see_signup_form.rb
@@ -0,0 +1,24 @@
+class Spinach::Features::MentorCanSeeSignupForm < Spinach::FeatureSteps
+  step 'Visitor is not a logged-in user' do
+    @mentor = nil
+    @student = nil
+  end
+
+  step 'he goes to the signup page' do
+    visit '/signup'
+  end
+
+  step 'he should see the signup form' do
+    assert page.has_css?('#signup-form')
+    
+    within('#signup-form') do
+      assert has_css?('#signup-form__name')
+      assert has_css?('#signup-form__email')
+      assert has_css?('#signup-form__password')
+    end
+    
+    filepath = __FILE__
+    name = File.basename(filepath, '.*')
+    page.save_screenshot("#{name}.png")
+  end
+end
