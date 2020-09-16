3abdc96ac431f9c50109fd8294ebf98823404754
Marvin Alejandro Herrera Vizuett <marvinahv@gmail.com>
2020-09-16 09:17:37 +0000

    Change capybara config for screenshots path and wait time

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,6 +10,8 @@ Capybara.configure do |config|
   config.run_server = false
   config.default_driver = :selenium_chrome_headless
   config.app_host = ENV['APP_HOST_URL'] # Set ENV variable
+  config.save_path = 'screenshots'
+  # config.default_max_wait_time = 5
 end
 
 Spinach::FeatureSteps.send(:include, Spinach::FeatureSteps::Capybara)
